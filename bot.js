93927c7aa132c13d6624623c24b8ba3cedd7ffbb
alexisloya_ <alexloy117@gmail.com>
2020-09-10 23:20:28 -0500

    fix hookPath

--- bot.js
+++ bot.js
@@ -9,9 +9,12 @@ bot.start((ctx) => ctx.reply('Welcome to lechitos'));
 bot.command('/image', (ctx) => {
     const id = ctx.message.chat.id;
     const url = "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.nuval.com.co%2Fwp-content%2Fuploads%2F2010%2F10%2Flechitos.jpg&f=1&nofb=1";
-    ctx.telegram.sendPhoto(id, url, {caption: 'Tu nueva imageb'});
+    ctx.telegram.sendPhoto(id, url, { caption: 'Tu nueva imageb' });
 });
 
 bot.launch({
-    webhook:{port:PORT}
+    webhook: {
+        port: PORT,
+        hookPath: "https://lechitos-bot.herokuapp.com"
+    }
 })
